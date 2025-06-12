d5631f3756118fc81bfacfaaaf27d42476368faa
김왕준 <qwejhk8520@gmail.com>
2025-06-12 07:31:13 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Button variant="g

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { AuthProvider } from '@/contexts/AuthContext';
+import { ThemeProvider } from 'next-themes'; // Added
 
 export const metadata: Metadata = {
   title: '인디 게임 개발자 커뮤니티',
@@ -14,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning> {/* Added suppressHydrationWarning for next-themes */}
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -22,10 +23,17 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Jua&family=Do+Hyeon&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light" // Sets initial theme to use :root styles (user's dark theme)
+          enableSystem={false} // Explicitly manage themes, don't rely on system for now
+          disableTransitionOnChange // Prevents style flashing on theme change
+        >
           <AuthProvider>
             {children}
             <Toaster />
           </AuthProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
